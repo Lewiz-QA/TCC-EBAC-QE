79cf7b819f6a5ccc93091b024e264998a7b1a9d6
Luiz <lgm97.com@gmail.com>
2024-01-27 00:52:32 -0300

    Testes Mobile CI

--- UI/automatizacao_cypress/cypress/support/pages/account.page.js
+++
@@ -1,6 +0,0 @@
-/// <reference types="cypress" />
-
-export const accountPage = {
-    get welcomeMessage() {return cy.get('.woocommerce-MyAccount-content > :nth-child(2) > :nth-child(1)')}
-}
-
--- UI/automatizacao_cypress/cypress/support/pages/index.js
+++
@@ -1,9 +0,0 @@
-/// <reference types="cypress" />
-
-import { accountPage } from './account.page.js'
-import LoginPage from './login.page.js'
-import ProductPage from './product.page.js'
-import ProductsPage from './products.page.js'
-import CartPage from './cart.page.js'
-
-export { accountPage, LoginPage, ProductPage, ProductsPage, CartPage }
